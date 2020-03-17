02ea0e0b79a87f0da58acbab8a01ae87f5be014a
stenfatt <56261000+stenfatt@users.noreply.github.com>
2020-03-17 12:34:44 +0100

    Update Board.cpp

--- src/Board.cpp
+++ src/Board.cpp
@@ -5,7 +5,9 @@
 
 using namespace std;
 
-Board::Board(){};
+Board::Board() {
+	Blue_turn = true;
+};
 
 //all pieces blue are negative. red pieces are positive
 
@@ -18,23 +20,23 @@ void Board::update_board(Board Playing){
 	}
 
 	for (unsigned int i = 0; i < Playing.Active.size(); i++) {
-		if (typeid(Playing.Active[i]).name() == "King"){
-			Playing.field[Playing.Active[i]->getX()][Playing.Active[i]->getY()] = Playing.Active[i]->getColour() * (Playing.Active[i]->getOrientation() * 10 + 0 );//Insert king expression;
+		if (typeid(Playing.Active[i][0]).name() == "King"){
+			Playing.field[Playing.Active[i]->getX()][Playing.Active[i]->getY()] = Playing.Active[i]->getColour() * (Playing.Active[i]->getOrientation()+50);//Insert king expression;
 		}
 
-		else if (typeid(Playing.Active[i]).name() == "Laser"){
-		Playing.field[Playing.Active[i]->getX()][Playing.Active[i]->getY()] = Playing.Active[i]->getColour() * (Playing.Active[i]->getOrientation() * 10 + 0 );//Insert Laser expression;
+		else if (typeid(Playing.Active[i][0]).name() == "Laser"){
+		Playing.field[Playing.Active[i]->getX()][Playing.Active[i]->getY()] = Playing.Active[i]->getColour() * (Playing.Active[i]->getOrientation()+10);//Insert Laser expression;
 		}
 
-		else if (typeid(Playing.Active[i]).name() == "Deflector"){
-		Playing.field[Playing.Active[i]->getX()][Playing.Active[i]->getY()] = Playing.Active[i]->getColour() * (Playing.Active[i]->getOrientation() * 10 + 0 );//Insert Deflector expression;
+		else if (typeid(Playing.Active[i][0]).name() == "Deflector"){
+		Playing.field[Playing.Active[i]->getX()][Playing.Active[i]->getY()] = Playing.Active[i]->getColour() * (Playing.Active[i]->getOrientation()+20);//Insert Deflector expression;
 		}
-		else if (typeid(Playing.Active[i]).name() == "Defender") {
-			Playing.field[Playing.Active[i]->getX()][Playing.Active[i]->getY()] = Playing.Active[i]->getColour() * (Playing.Active[i]->getOrientation() * 10 + 0);//Insert Defender expression;
+		else if (typeid(Playing.Active[i][0]).name() == "Defender") {
+			Playing.field[Playing.Active[i]->getX()][Playing.Active[i]->getY()] = Playing.Active[i]->getColour() * (Playing.Active[i]->getOrientation()+30);//Insert Defender expression;
 		}
 
-		else if (typeid(Playing.Active[i]).name() == "Switch") {
-			Playing.field[Playing.Active[i]->getX()][Playing.Active[i]->getY()] = Playing.Active[i]->getColour() * (Playing.Active[i]->getOrientation() * 10 + 0);//Insert Switch expression;
+		else if (typeid(Playing.Active[i][0]).name() == "Switch") {
+			Playing.field[Playing.Active[i]->getX()][Playing.Active[i]->getY()] = Playing.Active[i]->getColour() * (Playing.Active[i]->getOrientation()+40);//Insert Switch expression;
 		}
 
 		else { cout << "Unknown piece"; }
@@ -49,65 +51,39 @@ void Board::display_board(Board Playing){
 
 void Board::init_ace(void) {
 	//Red pieces
-	King RK(5, 7, S, 1);
-	Laser RL(S, 1);
-	Defender RD1(4, 7, S, 1);
-	Defender RD2(6, 7, S, 1);
-	Deflector RDL1(0, 3, E, 1);
-	Deflector RDL2(0, 4, N, 1);
-	Deflector RDL3(3, 6, S, 1);
-	Deflector RDL4(6, 2, E, 1);
-	Deflector RDL5(7, 3, N, 1);
-	Deflector RDL6(7, 4, E, 1);
-	Deflector RDL7(7, 7, E, 1);
-	Switch RS1(4, 4, N, 1);
-	Switch RS2(5, 4, E, 1);
 
-	Active.push_back(&RK);
-	Active.push_back(&RL);
-	Active.push_back(&RD1);
-	Active.push_back(&RD2);
+	Active.push_back(new King(5,7,S,1));
+	Active.push_back(new Laser(S, 1));
+	Active.push_back(new Defender(4, 7, S, 1));
+	Active.push_back(new Defender(6, 7, S, 1));
 
-	Active.push_back(&RDL1);
-	Active.push_back(&RDL2);
-	Active.push_back(&RDL3);
-	Active.push_back(&RDL4);
-	Active.push_back(&RDL5);
-	Active.push_back(&RDL6);
-	Active.push_back(&RDL7);
+	Active.push_back(new Deflector(0, 3, E, 1));
+	Active.push_back(new Deflector(0, 4, N, 1));
+	Active.push_back(new Deflector(3, 6, S, 1));
+	Active.push_back(new Deflector(6, 2, E, 1));
+	Active.push_back(new Deflector(7, 3, N, 1));
+	Active.push_back(new Deflector(7, 4, E, 1));
+	Active.push_back(new Deflector(7, 7, E, 1));
 
-	Active.push_back(&RS1);
-	Active.push_back(&RS2);
+	Active.push_back(new Switch(4, 4, N, 1));
+	Active.push_back(new Switch(5, 4, E, 1));
 
 	//Blue pieces
-	King BK(5, 7, S, -1);
-	Laser BL(S, -1);
-	Defender BD1(4, 7, S, -1);
-	Defender BD2(6, 7, S, -1);
-	Deflector BDL1(0, 3, E, -1);
-	Deflector BDL2(0, 4, N, -1);
-	Deflector BDL3(3, 6, S, -1);
-	Deflector BDL4(6, 2, E, -1);
-	Deflector BDL5(7, 3, N, -1);
-	Deflector BDL6(7, 4, E, -1);
-	Deflector BDL7(7, 7, E, -1);
-	Switch BS1(4, 4, N, -1);
-	Switch BS2(5, 4, E, -1);
 
-	Active.push_back(&BK);
-	Active.push_back(&BL);
-	Active.push_back(&BD1);
-	Active.push_back(&BD2);
+	Active.push_back(new King(5, 7, S, -1));
+	Active.push_back(new Laser(S, -1));
+	Active.push_back(new Defender(4, 7, S, -1));
+	Active.push_back(new Defender(6, 7, S, -1));
 
-	Active.push_back(&BDL1);
-	Active.push_back(&BDL2);
-	Active.push_back(&BDL3);
-	Active.push_back(&BDL4);
-	Active.push_back(&BDL5);
-	Active.push_back(&BDL6);
-	Active.push_back(&BDL7);
+	Active.push_back(new Deflector(0, 3, E, -1));
+	Active.push_back(new Deflector(0, 4, N, -1));
+	Active.push_back(new Deflector(3, 6, S, -1));
+	Active.push_back(new Deflector(6, 2, E, -1));
+	Active.push_back(new Deflector(7, 3, N, -1));
+	Active.push_back(new Deflector(7, 4, E, -1));
+	Active.push_back(new Deflector(7, 7, E, -1));
 
-	Active.push_back(&BS1);
-	Active.push_back(&BS2);
+	Active.push_back(new Switch(4, 4, N, -1));
+	Active.push_back(new Switch(5, 4, E, -1));
 	return;
 }
