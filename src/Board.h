55d461f3f52f70855f8d67c138d0291111da8698
stenfatt <56261000+stenfatt@users.noreply.github.com>
2020-03-17 09:24:36 +0100

    Update Board.h

--- src/Board.h
+++ src/Board.h
@@ -7,14 +7,14 @@
 class Board {
 protected:
 	int field[8][10];
-	vector<piece *> Active;
-
+	std::vector <piece*> Active;
 public:
 	Board();
 	~Board();
-	int* update_board(Board Playing);
+	void update_board(Board Playing);
 	void display_board(Board Playing);
 	void init_ace(void);
 };
+
 
 #endif Board_H_
