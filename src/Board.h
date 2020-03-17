3a720132b38e71f7edfd7e5ead02e7d21fc5d118
stenfatt <56261000+stenfatt@users.noreply.github.com>
2020-03-17 08:22:49 +0100

    Update Board.h

--- src/Board.h
+++ src/Board.h
@@ -1,15 +1,13 @@
 #ifndef Board_H_
 #define Board_H_
 
-#include <iostream> 
 #include <vector> 
-#include <string>
 #include "piece.h"
 
 class Board {
 protected:
 	int field[8][10];
-	vector<piece*> Active;
+	vector<piece *> Active;
 
 public:
 	Board();
@@ -18,6 +16,5 @@ public:
 	void display_board(Board Playing);
 	void init_ace(void);
 };
-
 
 #endif Board_H_
